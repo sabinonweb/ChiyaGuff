2cbcc263ccab3eb3b4b47bc2dac1af204618678c
Sabin <sabinonweb7@gmail.com>
2023-08-12 07:42:33 +0545

    Halfway There

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const express = express.require;
+const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+// app.set('views', 'views') : specify where to look
+
+app.listen("3000");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { title: "Home" });
+});
+
+app.get("/create/blog", (req, res) => {
+  res.render("create", { title: "Create Blog" });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/about-us", (req, res) => {
+  res.redirect("about");
+});
+
+app.use((req, res) => {
+  res.status(404).render("404", { title: "Dumbo" });
 });
